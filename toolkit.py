72922bef5fb904c00bf29bf863933e50c463d642
Malcolm Lett <malcolm.lett@gmail.com>
2024-12-14 08:54:06 +0530

    Clean but basic version of custom training loop for gradient collection

--- toolkit.py
+++ toolkit.py
@@ -55,14 +55,101 @@ class LessVerboseProgressLogger(tf.keras.callbacks.Callback):
             return f"{int(mins)}m {secs:.2f}s"
 
 
+# Tries to replicate keras.backend.tensorflow.TensorFlowTrainer.fit() (trainer.py, keras 3.5.0)
+# as much as possible.
+def fit(model, dataset, epochs=1, verbose=1, callbacks=None, initial_epoch=0, gradient_callback=None):
+    """
+    A custom training loop mimicking model.fit() that makes gradient information available for tracking.
 
-def train_step(model, x_batch_train, y_batch_train, sample_weight):
-    reported_loss = None
+    Honours the state of `tf.config.run_functions_eagerly(bool)`.
+
+    Args:
+        model: usual meaning
+        dataset: usual meaning
+        epochs: usual meaning
+        verbose: usual meaning
+        callbacks: usual meaning
+        initial_epoch: usual meaning
+        gradient_callback: Supply a subclass instance of BaseGradientCallback in order to collect gradient
+            information during training.
+    Returns:
+         HistoryCallback
+    """
+    # prepare epochs
+    num_batches = len(dataset)
+
+    # prepare callbacks tracking
+    if not isinstance(callbacks, tf.keras.callbacks.CallbackList):
+        callbacks = tf.keras.callbacks.CallbackList(callbacks, add_history=True, add_progbar=verbose != 0,
+                                                    verbose=verbose, epochs=epochs, steps=num_batches, model=model)
+    if gradient_callback is None:
+        gradient_callback = BaseGradientCallback()
+    gradient_callback.set_params({'epochs': epochs, 'steps': len(dataset)})
+    gradient_callback.set_model(model)
+
+    # prepare train function
+    if tf.config.functions_run_eagerly():
+        print(f"Execution mode: eager")
+        train_step_fn = _gradient_returning_train_step
+    else:
+        print(f"Execution mode: autograph")
+        train_step_fn = tf.function(_gradient_returning_train_step)
+
+    # train
+    logs = {}  # holds latest value at any given moment in time
+    callbacks.on_train_begin()
+    gradient_callback.on_train_begin()
+    for epoch in range(initial_epoch, epochs):
+        model.reset_metrics()
+        start = tf.timestamp()
+        callbacks.on_epoch_begin(epoch)
+        gradient_callback.on_epoch_begin(epoch)
+
+        for step, (x_batch_train, y_batch_train) in enumerate(dataset):
+            sample_weight = None  # TODO use: x, y, sample_weight = data_adapter_utils.unpack_x_y_sample_weight(data)
+            # print(f"  Step {step+1}: x_batch_train: {x_batch_train.shape}, y_batch_train: {y_batch_train.shape}")
+            callbacks.on_train_batch_begin(step)
+            gradient_callback.on_train_batch_begin(step)
+
+            loss, metrics, gradients = train_step_fn(model, x_batch_train, y_batch_train, sample_weight)
+
+            logs = metrics
+            logs['loss'] = loss.numpy()
+            callbacks.on_train_batch_end(step, logs)
+            gradient_callback.on_train_batch_end(step, loss, gradients, model.trainable_variables, None)
+
+        # end of epoch
+        dur = (tf.timestamp() - start).numpy()
+        callbacks.on_epoch_end(epoch, logs)  # should be passing loss and mse
+        gradient_callback.on_epoch_end(epoch, loss, gradients, model.trainable_variables, None)
+        metric_str = ''
+        for k in logs.keys():
+            metric_str += f" - {k}: {logs[k]:.3f}"
+    callbacks.on_train_end(logs)
+    gradient_callback.on_train_end()
+
+    return model.history
+
+
+# Tries to replicate keras.backend.tensorflow.TensorFlowTrainer.train_step() (trainer.py, keras 3.5.0)
+# as much as possible.
+def _gradient_returning_train_step(model, x_batch_train, y_batch_train, sample_weight):
+    """
+    This method is programmatically converted via auto-graph.
+
+    Returns:
+        loss: float. Loss returned by loss function (before optimizer scaling).
+        metrics: dict. Metrics returned by model (note: also includes a 'loss' value but it's always zero)
+        gradients: list. Gradients tensor for each trainable variable
+    """
 
     # Forward pass
     with tf.GradientTape() as tape:
         y_batch_pred = model(x_batch_train)
-        loss = model.compute_loss(x=x_batch_train, y=y_batch_train, y_pred=y_batch_pred, sample_weight=sample_weight,
+        loss = model.compute_loss(x=x_batch_train,
+                                  y=y_batch_train,
+                                  y_pred=y_batch_pred,
+                                  sample_weight=sample_weight,
                                   training=True)
         reported_loss = loss  # tracking before scaling
         loss = model.optimizer.scale_loss(loss)
@@ -81,60 +168,89 @@ def train_step(model, x_batch_train, y_batch_train, sample_weight):
     return reported_loss, metrics, gradients
 
 
-# Honours tf.config.run_functions_eagerly(bool)
-# See tensorflow trainer.Trainer.train_step() for reference
-def fit(model, dataset, epochs=1, verbose=1, callbacks=None, initial_epoch=0):
-    # prepare epochs
-    num_batches = len(dataset)
+class BaseGradientCallback:
+    """
+    Supply a subclass instance to the custom fit() method in order to collect gradient information.
+    """
+    def __init__(self):
+        self.params = None
+        self._model = None
 
-    # prepare callbacks tracking
-    # if verbose >= 1:
-    #  history = tf.keras.callbacks.History()
-    #  callbacks.append(history)
-    if not isinstance(callbacks, tf.keras.callbacks.CallbackList):
-        callbacks = tf.keras.callbacks.CallbackList(callbacks, add_history=True, add_progbar=verbose != 0,
-                                                    verbose=verbose, epochs=epochs, steps=num_batches, model=model)
+    def set_params(self, params):
+        self.params = params
 
-    # prepare train function
-    if tf.config.functions_run_eagerly():
-        print(f"Execution mode: eager")
-        train_step_fn = train_step
-    else:
-        print(f"Execution mode: autograph")
-        train_step_fn = tf.function(train_step)
+    def set_model(self, model):
+        self._model = model
 
-    # start
-    callbacks.set_params({'verbose': 1, 'epochs': epochs, 'steps': len(dataset)})
+    @property
+    def model(self):
+        return self._model
 
-    # train
-    # gradients_list = []
-    logs = {}  # holds latest value at any given moment in time
-    callbacks.on_train_begin()
-    for epoch in range(initial_epoch, epochs):
-        model.reset_metrics()
-        start = tf.timestamp()
-        callbacks.on_epoch_begin(epoch)
+    def on_train_begin(self):
+        """Called at the beginning of training.
 
-        for step, (x_batch_train, y_batch_train) in enumerate(dataset):
-            sample_weight = None  # TODO use: x, y, sample_weight = data_adapter_utils.unpack_x_y_sample_weight(data)
-            # print(f"  Step {step+1}: x_batch_train: {x_batch_train.shape}, y_batch_train: {y_batch_train.shape}")
-            callbacks.on_train_batch_begin(step)
+        Subclasses should override for any actions to run.
+        """
 
-            loss, metrics, gradients = train_step_fn(model, x_batch_train, y_batch_train, sample_weight)
+    def on_train_end(self):
+        """Called at the end of training.
 
-            # gradients_list.append(gradients)
-            logs = metrics
-            logs['loss'] = loss.numpy()
-            callbacks.on_train_batch_end(step, logs)
+        Subclasses should override for any actions to run.
+        """
 
-        # end of epoch
-        dur = (tf.timestamp() - start).numpy()
-        callbacks.on_epoch_end(epoch, logs)  # should be passing loss and mse
-        metric_str = ''
-        for k in logs.keys():
-            metric_str += f" - {k}: {logs[k]:.3f}"
-        # print(f"Epoch {epoch+1} - {dur:.1f}s - loss: {logs['loss']:.3f}{metric_str}")
-    callbacks.on_train_end(logs)
+    def on_epoch_begin(self, epoch):
+        """Called at the start of an epoch.
 
-    # return history, gradients_list
-    return model.history
+        Subclasses should override for any actions to run.
+
+        Args:
+            epoch: Integer, index of epoch.
+        """
+
+    def on_epoch_end(self, epoch, loss, gradients, trainable_variables, activations=None):
+        """Called at the end of an epoch during training.
+        Subclasses should override for any actions to run.
+
+        Supplied with training parameters from the last batch of the epoch.
+        Can be a convenient alternative where training doesn't use batches,
+        or when you only want to sample the occasional update.
+
+        Args:
+            epoch: Integer, index of epoch.
+            loss: float. The loss value of the batch.
+            gradients: the list of gradients for each trainable variable.
+            trainable_variables: the list of trainable variables.
+            activations: the list of activations for each layer. Currently
+              never populated, but might be in the future.
+        """
+
+    def on_train_batch_begin(self, batch):
+        """Called at the beginning of a training batch in `fit` methods.
+
+        Subclasses should override for any actions to run.
+
+        Note that if the `steps_per_execution` argument to `compile` in
+        `Model` is set to `N`, this method will only be called every
+        `N` batches.
+
+        Args:
+            batch: Integer, index of batch within the current epoch.
+        """
+
+    def on_train_batch_end(self, batch, loss, gradients, trainable_variables, activations=None):
+        """Called at the end of a training batch in `fit` methods.
+
+        Subclasses should override for any actions to run.
+
+        Note that if the `steps_per_execution` argument to `compile` in
+        `Model` is set to `N`, this method will only be called every
+        `N` batches.
+
+        Args:
+            batch: Integer, index of batch within the current epoch.
+            loss: float. The loss value of the batch.
+            gradients: the list of gradients for each trainable variable.
+            trainable_variables: the list of trainable variables.
+            activations: the list of activations for each layer. Currently
+              never populated, but might be in the future.
+        """
