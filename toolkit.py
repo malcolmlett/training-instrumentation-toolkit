e5f4d25bcccc6f87ab66eaa345afb5a39a101b73
Malcolm Lett <malcolm.lett@gmail.com>
2024-12-14 08:21:45 +0530

    Alternative progres logger on smaller models

---
+++ toolkit.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import math
+
+
+class LessVerboseEpochLogger(tf.keras.callbacks.Callback):
+    """
+    Progress logger for when running models that train via thousands of very short epochs.
+    By default, automatically logs progress 10 times during training.
+
+    Use as:
+    >>> model.fit(...., verbose=0, callbacks=[LessVerboseEpochLogger()])
+    """
+    def __init__(self, display_interval=None, display_total=10):
+        super(LessVerboseEpochLogger, self).__init__()
+        self.display_interval = display_interval
+        self.display_total = display_total
+        self.epoch_count = None
+        self.group_start_time = None
+        self.group_start_epoch = None
+        self.epoch_start = None
+
+    def set_params(self, params):
+        self.epoch_count = params['epochs']
+        self.group_start_epoch = 0
+        self.group_start_time = tf.timestamp()
+        if self.display_interval is None:
+            self.display_interval = math.floor(self.epoch_count / self.display_total)
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch_start = tf.timestamp()
+
+    def on_epoch_end(self, epoch, logs=None):
+        if ((epoch + 1) % self.display_interval == 0) or epoch == self.epoch_count-1:
+            now = tf.timestamp()
+            group_dur = now - self.group_start_time
+            rate = group_dur / (epoch - self.group_start_epoch)
+            self.group_start_time = now
+            self.group_start_epoch = epoch
+
+            print(f'Epoch {epoch + 1:5d} - {self._format_duration(rate)}/epoch:', end=' ')
+            for key, value in logs.items():
+                print(f'{key}: {value:.4f}', end='  ')
+            print()
+
+    @staticmethod
+    def _format_duration(seconds):
+        if seconds < 1e-3:
+            return f"{seconds * 1e6:.2f}µs"
+        elif seconds < 1:
+            return f"{seconds * 1e3:.2f}ms"
+        elif seconds < 60:
+            return f"{seconds:.2f}s"
+        else:
+            mins, secs = divmod(seconds, 60)
+            return f"{int(mins)}m {secs:.2f}s"
+
