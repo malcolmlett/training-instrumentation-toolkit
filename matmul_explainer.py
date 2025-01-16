2fd6d9e1083a610c0242197616f508efe05a7d95
Malcolm Lett <malcolm.lett@gmail.com>
2025-01-16 10:59:52 +0530

    Now with conv explainer

--- matmul_explainer.py
+++ matmul_explainer.py
@@ -11,6 +11,8 @@
 # of a given computation to explain its output.
 
 import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
 
 
 def classify_terms():
@@ -33,7 +35,7 @@ def matmul_classify(x1, x2, confidence: float = 0.95, threshold1: float = None, 
     For the entire result this can be computed via:
       output = np.sum(sums, axis=-1)
 
-    "Near zero" is defined as any value having an absolute value less than or equal a threshold.
+    "Near zero" is defined as any value having an absolute value less than a threshold.
     By default, the threshold is determined by taking the (1 - zero_confidence) percentile level across each
     of the separate inputs. Alternatively, an absolute magnitude threshold may be specified for
     any of the inputs.
@@ -57,8 +59,10 @@ def matmul_classify(x1, x2, confidence: float = 0.95, threshold1: float = None, 
         that a value is accurately placed within the P, Z, or N categories.
         Higher values lead to more strict requirements for "near zero".
         1.0 only considers exactly 0.0 as "near zero".
-      threshold1: abs(X1) values less than or equal this are considered near-zero
-      threshold2: abs(X2) values less than or equal this are considered near-zero
+      threshold1: abs(X1) values less than this are considered near-zero,
+        otherwise inferred from confidence
+      threshold2: abs(X2) values less than this are considered near-zero,
+        otherwise inferred from confidence
 
     Returns:
       (counts, sums) where each is an np-array with shape (n, m, 9)
@@ -69,21 +73,20 @@ def matmul_classify(x1, x2, confidence: float = 0.95, threshold1: float = None, 
     x2 = np.array(x2)
 
     # determine thresholds
-    # (note: on small matrices with few discrete numbers, np.percentile will find a value <= percentile_level,
-    #  so subsequently we need to use this threshold via the rule "zero if value <= threshold", otherwise we might
-    #  miss values on the threshold that are still < percentile_level.)
+    # (note: on small matrices with few discrete numbers, np.percentile will find a value on either side
+    #  of the percentage threshold, thus we should apply the threshold rule as "zero if value < threshold"
     if threshold1 is None:
-        threshold1 = np.percentile(np.abs(x1), 100 * (1 - confidence))
+        threshold1 = np.percentile(np.abs(x1), 100 * (1 - confidence), method='midpoint')
     if threshold2 is None:
-        threshold2 = np.percentile(np.abs(x2), 100 * (1 - confidence))
+        threshold2 = np.percentile(np.abs(x2), 100 * (1 - confidence), method='midpoint')
 
     # create masks that classify each input individually
     x1_p = x1 > threshold1
-    x1_z = np.abs(x1) <= threshold1
+    x1_z = np.abs(x1) < threshold1
     x1_n = x1 < -threshold1
 
     x2_p = x2 > threshold2
-    x2_z = np.abs(x2) <= threshold2
+    x2_z = np.abs(x2) < threshold2
     x2_n = x2 < -threshold2
 
     # compute counts
@@ -111,3 +114,96 @@ def matmul_classify(x1, x2, confidence: float = 0.95, threshold1: float = None, 
     sums[:, :, 8] = np.matmul(x1 * x1_n, x2 * x2_n)
 
     return counts, sums
+
+
+def conv_classify(inputs, kernel, strides=1, padding="VALID", confidence: float = 0.95, inputs_threshold: float = None, kernel_threshold: float = None):
+    """
+    Like matmul_classify but for convolutions.
+    Supports 1D, 2D and 3D convolution.
+
+    Args:
+        inputs: Tensor of rank N+2. `inputs` has shape
+            `(batch_size,) + inputs_spatial_shape + (num_channels,)`
+        kernel: Tensor of rank N+2. `kernel` has shape
+            `(kernel_spatial_shape, num_input_channels, num_output_channels)`.
+            `num_input_channels` should match the number of channels in
+            `inputs`.
+        strides: int or int tuple/list of `len(inputs_spatial_shape)`,
+            specifying the strides of the convolution along each spatial
+            dimension. If `strides` is int, then every spatial dimension shares
+            the same `strides`.
+        padding: string, either `"valid"` or `"same"`. `"valid"` means no
+            padding is applied, and `"same"` results in padding evenly to the
+            left/right or up/down of the input such that output has the
+            same height/width dimension as the input when `strides=1`.
+        confidence: statistical confidence (0.0 to 1.0) that you wish to meet
+          that a value is accurately placed within the P, Z, or N categories.
+          Higher values lead to more strict requirements for "near zero".
+          1.0 only considers exactly 0.0 as "near zero".
+        inputs_threshold: abs(X1) values less than this are considered near-zero,
+          otherwise inferred from confidence
+        kernel_threshold: abs(X2) values less than this are considered near-zero,
+          otherwise inferred from confidence
+
+    Returns:
+        (counts, sums) containing the counts and sums of each component, respectively.
+        Each a tensor with shape `(batch_size,) + inputs_spatial_shape + (num_channels,9)`.
+    """
+    # standardise on data format
+    inputs = tf.constant(inputs)
+    kernel = tf.constant(kernel)
+
+    # determine thresholds
+    # (note: on small matrices with few discrete numbers, percentile() will find a value on either side
+    #  of the percentage threshold, thus we should apply the threshold rule as "zero if value < threshold"
+    if inputs_threshold is None:
+        inputs_threshold = tfp.stats.percentile(tf.abs(inputs), 100 * (1 - confidence), interpolation='midpoint')
+    if kernel_threshold is None:
+        kernel_threshold = tfp.stats.percentile(tf.abs(kernel), 100 * (1 - confidence), interpolation='midpoint')
+
+    # create masks that classify each input individually
+    inputs_p = inputs > inputs_threshold
+    inputs_z = np.abs(inputs) < inputs_threshold
+    inputs_n = inputs < -inputs_threshold
+
+    kernel_p = kernel > kernel_threshold
+    kernel_z = np.abs(kernel) < kernel_threshold
+    kernel_n = kernel < -kernel_threshold
+
+    # compute counts and sums for each classification
+    counts = []
+    inputs_pc = tf.cast(inputs_p, tf.float32)
+    inputs_zc = tf.cast(inputs_z, tf.float32)
+    inputs_nc = tf.cast(inputs_n, tf.float32)
+    kernel_pc = tf.cast(kernel_p, tf.float32)
+    kernel_zc = tf.cast(kernel_z, tf.float32)
+    kernel_nc = tf.cast(kernel_n, tf.float32)
+    counts.append(tf.nn.convolution(input=inputs_pc, filters=kernel_pc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_pc, filters=kernel_zc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_pc, filters=kernel_nc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_zc, filters=kernel_pc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_zc, filters=kernel_zc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_zc, filters=kernel_nc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_nc, filters=kernel_pc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_nc, filters=kernel_zc, strides=strides, padding=padding))
+    counts.append(tf.nn.convolution(input=inputs_nc, filters=kernel_nc, strides=strides, padding=padding))
+
+    sums = []
+    inputs_pv = tf.where(inputs_p, inputs, tf.zeros_like(inputs))
+    inputs_zv = tf.where(inputs_z, inputs, tf.zeros_like(inputs))
+    inputs_nv = tf.where(inputs_n, inputs, tf.zeros_like(inputs))
+    kernel_pv = tf.where(kernel_p, kernel, tf.zeros_like(kernel))
+    kernel_zv = tf.where(kernel_z, kernel, tf.zeros_like(kernel))
+    kernel_nv = tf.where(kernel_n, kernel, tf.zeros_like(kernel))
+    sums.append(tf.nn.convolution(input=inputs_pv, filters=kernel_pv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_pv, filters=kernel_zv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_pv, filters=kernel_nv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_zv, filters=kernel_pv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_zv, filters=kernel_zv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_zv, filters=kernel_nv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_nv, filters=kernel_pv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_nv, filters=kernel_zv, strides=strides, padding=padding))
+    sums.append(tf.nn.convolution(input=inputs_nv, filters=kernel_nv, strides=strides, padding=padding))
+
+    # format into final output
+    return tf.stack(counts, axis=-1), tf.stack(sums, axis=-1)
--- matmul_explainer_test.py
+++ matmul_explainer_test.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def run_test_suite():
     matmul_classify_test()
+    conv_classify_2d_test()
 
 
 def matmul_classify_test():
@@ -19,4 +20,33 @@ def matmul_classify_test():
     real_matmul = np.matmul(a, a)
     derived_matmul = np.sum(sums, axis=-1)
     assert np.allclose(real_matmul, derived_matmul), "real matmul and derived matmul are different"
+
+
+def conv_classify_2d_test():
+    a = np.arange(0.0, 0.9, 0.1)
+    a = np.tile(a, (9, 1)).astype(np.float32)
+    a = tf.reshape(a, shape=(1, 9, 9, 1))
+
+    k = np.array([
+        [-1, 0, -1],
+        [+1, +1, +1],
+        [-1, 0, -1]
+    ]).astype(np.float32)
+    k = tf.reshape(k, shape=(3, 3, 1, 1))
+
+    counts, sums = conv_classify(a, k, confidence=0.95)
+
+    expected_counts = [140, 0, 182, 0, 0, 0, 0, 0, 0]
+    actual_counts = np.sum(counts, axis=(0, 1, 2, 3))
+    assert np.all(actual_counts == expected_counts), f"Expected counts {expected_counts}, got: {actual_counts}"
+
+    expected_sums = [58.79999, 0., -78.40001, 0., 0., 0., 0., 0., 0.]
+    actual_sums = np.sum(sums, axis=(0, 1, 2, 3))
+    assert np.allclose(actual_sums == expected_sums), f"Expected sums {expected_sums}, got: {actual_sums}"
+
+    real_matmul = np.matmul(a, a)
+    derived_matmul = np.sum(sums, axis=-1)
+    expected_conv = tf.nn.convolution(a, k)
+    derived_conv = tf.reduce_sum(sums, axis=-1)
+    assert np.allclose(expected_conv, derived_conv), "real conv and derived conv are different"
 
