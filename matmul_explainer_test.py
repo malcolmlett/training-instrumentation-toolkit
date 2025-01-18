74d25673e05eba9e17915f32856a90299c5d4bb3
Malcolm Lett <malcolm.lett@gmail.com>
2025-01-18 10:35:12 +0530

    Extended matmul-explainer to single tensors

---
+++ matmul_explainer_test.py
@@ -0,0 +1,79 @@
+from matmul_explainer import *
+import tensorflow as tf
+import numpy as np
+
+
+def run_test_suite():
+    matmul_classify_test()
+    conv_classify_1d_test()
+    conv_classify_2d_test()
+    print("All matmul_explainer tests passed.")
+
+
+def matmul_classify_test():
+    a = np.arange(0.0, 1.0, 0.1)
+    a = np.tile(a, (10, 1))
+
+    counts, sums = matmul_classify(a, a, confidence=0.75)
+
+    expected_counts = [640, 160, 0, 160, 40, 0, 0, 0, 0]
+    actual_counts = np.sum(counts, axis=(0, 1))
+    assert np.all(actual_counts == expected_counts), f"Expected counts {expected_counts}, got: {actual_counts}"
+
+    expected_sums = [193.6, 4.4, 0, 4.4, 0.1, 0., 0., 0., 0.]
+    actual_sums = np.sum(sums, axis=(0, 1))
+    assert np.allclose(actual_sums, expected_sums), f"Expected sums {expected_sums}, got: {actual_sums}"
+
+    real_matmul = np.matmul(a, a)
+    derived_matmul = np.sum(sums, axis=-1)
+    assert np.allclose(real_matmul, derived_matmul), "real matmul and derived matmul are different"
+
+
+def conv_classify_1d_test():
+    a = np.arange(0.0, 0.9, 0.1).astype(np.float32)
+    a = tf.reshape(a, shape=(1,9,1))
+
+    k = np.array([-1, 0, -1]).astype(np.float32)
+    k = tf.reshape(k, shape=(3,1,1))
+
+    counts, sums = conv_classify(a, k, confidence=0.90)
+
+    expected_counts = [0, 7, 13, 0, 0, 1, 0, 0, 0]
+    actual_counts = np.sum(counts, axis=(0, 1, 2))
+    assert np.all(actual_counts == expected_counts), f"Expected counts {expected_counts}, got: {actual_counts}"
+
+    expected_sums = [0., 0., -5.6, 0., 0., 0., 0., 0., 0.]
+    actual_sums = np.sum(sums, axis=(0, 1, 2))
+    assert np.allclose(actual_sums, expected_sums), f"Expected sums {expected_sums}, got: {actual_sums}"
+
+    expected_conv = tf.nn.convolution(a, k)
+    derived_conv = tf.reduce_sum(sums, axis=-1)
+    assert np.allclose(expected_conv, derived_conv), "real conv and derived conv are different"
+
+
+def conv_classify_2d_test():
+    a = np.arange(0.0, 0.9, 0.1)
+    a = np.tile(a, (9, 1)).astype(np.float32)
+    a = tf.reshape(a, shape=(1, 9, 9, 1))
+
+    k = np.array([
+        [-1, 0, -1],
+        [+1, +1, +1],
+        [-1, 0, -1]
+    ]).astype(np.float32)
+    k = tf.reshape(k, shape=(3, 3, 1, 1))
+
+    counts, sums = conv_classify(a, k, confidence=0.75)
+
+    expected_counts = [126, 84, 168, 21, 14, 28, 0, 0, 0]
+    actual_counts = np.sum(counts, axis=(0, 1, 2, 3))
+    assert np.all(actual_counts == expected_counts), f"Expected counts {expected_counts}, got: {actual_counts}"
+
+    expected_sums = [57.4, 0., -77., 1.4000002, 0., -1.4000001, 0., 0., 0.,]
+    actual_sums = np.sum(sums, axis=(0, 1, 2, 3))
+    assert np.allclose(actual_sums, expected_sums), f"Expected sums {expected_sums}, got: {actual_sums}"
+
+    expected_conv = tf.nn.convolution(a, k)
+    derived_conv = tf.reduce_sum(sums, axis=-1)
+    assert np.allclose(expected_conv, derived_conv), "real conv and derived conv are different"
+
