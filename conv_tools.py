b008093b552cd97e0d74d0bea927a5af734ee74a
Malcolm Lett <malcolm.lett@gmail.com>
2025-01-31 17:04:51 +0530

    conv_backprop_filter()

--- conv_tools.py
+++ conv_tools.py
@@ -3,6 +3,151 @@ import tensorflow as tf
 import numpy as np
 
 
+def conv_backprop_filter(x, d_out, kernel_shape, strides, padding, data_format='NHWC'):
+    """
+    Computes the gradients of N-D convolution with respect to the kernel.
+
+    Support matrix:
+    - supported: strides of up to 2
+    - unsupported: strides > 2
+    - unsupported: dilations
+    - only 'NHWC' data format is supported
+
+    This implementation was initially inspired by the following work by Yixing Lao, 2017:
+    - https://gist.github.com/yxlao/ef50416011b9587835ac752aa3ce3530
+    - https://stackoverflow.com/questions/39373230/what-does-tensorflows-conv2d-transpose-operation-do/44350789#44350789
+    However that initial code was written for TensorFlow v2 and no longer worked. The final solution
+    was devised from a lengthy round of trial-and-error. It may not be theoretically sound, but it seems to
+    work well enough on the situations that it can handle.
+
+    Args:
+        x: original input tensor that had one of the `tf.nn.conv` operations applied against it.
+        kernel_shape: tensor, list, or tuple identifying the shape of the filter used during convolution.
+        d_out: tensor containing the `dJ/dOut` backprop gradients
+        strides: scalar, list or tuple of strides in each spatial dimension. Can omit the batch and channel dims,
+          which must have stride 1 if included.
+        padding: one of 'VALID' or 'SAME'
+        data_format: must be 'NHWC'
+    Returns:
+        tensor containing the `dJ/dW` gradients
+    """
+    if strides is None:
+        strides = [1] * (len(x.shape)-2)
+    elif isinstance(strides, (int, float, complex, str, bool)):
+        strides = [strides] * (len(x.shape)-2)
+    if padding is None:
+        padding = 'VALID'
+
+    if x.shape[0] != d_out.shape[0]:
+        raise ValueError(f"Batch dims don't match between x and d_out, got: x={x.shape}, d_out={d_out.shape}")
+    if len(x.shape) != len(d_out.shape):
+        raise ValueError(f"Number of dims of x and d_out must match, got: x={x.shape}, d_out={d_out.shape}")
+    if x.shape[-1] != kernel_shape[-2]:
+        raise ValueError(f"Input channel dim doesn't match kernel input channel dim, got: x={x.shape}, kernel={kernel_shape}")
+    if d_out.shape[-1] != kernel_shape[-1]:
+        raise ValueError(f"Output channel dim doesn't match kernel output channel dim, got: d_out={d_out.shape}, kernel={kernel_shape}")
+    if len(strides) != len(x.shape)-2 and len(strides) != len(x.shape):
+        raise ValueError(f"Number of strides must match tensor dims or spatial dims: "
+                         f"{len(strides)} doesn't match {x.shape}")
+    if len(strides) == len(x.shape) and (strides[0] != 1 or strides[-1] != 1):
+        raise ValueError(f"Strides for batch and channel dims must be 1, got: {strides}")
+    if padding is not None and padding not in ('SAME', 'VALID'):
+        raise ValueError(f"Unsupported padding: {padding}")
+    if data_format != 'NHWC':
+        raise ValueError("Only NHWC data format supported")
+
+    # get meta-info on the dimensions
+    x_spatial_shape = np.array(x.shape[1:-1])          # (batch, ..spatial_dims.., channel)
+    k_spatial_shape = np.array(kernel_shape[:-2])      # (..spatial_filters.., in_channel, out_channel)
+    spatial_strides = strides if len(strides) == len(x_spatial_shape) else strides[1:-1]
+
+    # apply padding to x
+    # - tip: more padding makes dJdW kernel larger
+    # - the following applies independently for each spatial dim:
+    #   - in general, if padding=SAME, then we need to distribute (k-1)/2 pad between before/after on each dim.
+    #   - in general, if padding=VALID, then we don't need to add padding;
+    #     however, when strides > 1, and k is even, then need to add a half-pad to one side
+    #   - when k and x are both even length, and strides > 1, then we need to flip the bias direction for padding.
+    x_paddings = []
+    for dim in range(len(x_spatial_shape)):
+        x_len = x_spatial_shape[dim]
+        k = k_spatial_shape[dim]
+        s = spatial_strides[dim]
+        if padding == 'SAME':
+            total = k - 1
+            before = total // 2  # usually biased towards 'after'
+            after = total - before
+            if k % 2 == 0 and x_len % 2 == 0 and s > 1:
+                # flip pad bias
+                after = total // 2
+                before = total - after
+        else:
+            before = 0
+            after = 0
+            if s > 1 and k % 2 == 0:
+                # half-pad, with bias depending on odd/even of x length
+                before, after = (0, 1) if x_len % 2 == 0 else (1, 0)
+        x_paddings.append([before, after])
+    x_paddings = [[0, 0]] + x_paddings + [[0, 0]]
+    x_prepared = tf.pad(x, x_paddings)
+    x_spatial_shape = np.array(x_prepared.shape[1:-1])  # revise for later processing
+
+    # handle strides
+    # - strides during the original convolution translate as "fractional strides" during backprop.
+    # - to achieve that effect, we dilate d_out.
+    d_out_prepared = dilate(d_out, spatial_strides)
+
+    # apply padding to d_out
+    # - tip: more padding makes dJdW kernel smaller
+    # - the following applies independently for each spatial dim:
+    #   - compute padding as: padding = prepared_x.shape - prepared_d_out.shape - kernel.shape + 1
+    #   - distribute between before/after with bias direction depending on whether x is_len is even or odd
+    d_out_spatial_shape = np.array(d_out_prepared.shape[1:-1])  # (batch, ..spatial_dims.., channel)
+    d_out_paddings = []
+    for dim in range(len(d_out_spatial_shape)):
+        d_len = d_out_spatial_shape[dim]
+        x_len = x_spatial_shape[dim]
+        k = k_spatial_shape[dim]
+        s = spatial_strides[dim]
+        before = after = 0
+        if s > 1:
+            total = x_len - d_len - k + 1
+            before = total // 2  # usually biased towards 'after'
+            after = total - before
+            if padding == 'SAME':
+                # flip pad bias
+                after = total // 2
+                before = total - after
+        d_out_paddings.append([before, after])
+    d_out_paddings = [[0, 0]] + d_out_paddings + [[0, 0]]
+    if (np.array(d_out_paddings) < 0).any():
+        raise ValueError(f"Invalid dimensions, got negative padding: {d_out_paddings}")
+    d_out_prepared = tf.pad(d_out_prepared, d_out_paddings)
+
+    # rearrange dims
+    # - we want to sum over the batch and spatial dimensions in order to compute dJdW
+    # - so we rearrange the dimensions to make that work
+    # Example pattern of rearrangements:
+    #             internal conv layout        API layout
+    #             --------------------     -----------------
+    #    input:   (  5, 120, 145, 32)  <-  (32, 120, 145,  5)  <- A_0
+    #    filters: (118, 143,  32, 16)  <-  (32, 118, 143, 16)  <- dJdZ
+    #    result:  (  5,   3,   3, 16)   -> ( 3,   3,   5, 16)   -> dJdW
+    x_prepared = tf_NHWC_to_CHWN(x_prepared)
+    d_out_prepared = tf_NHWC_to_HWIO(d_out_prepared)
+
+    # compute convolution
+    # - natively supports any-dimensional convolution
+    # - no strides and no padding, as that's already been taken care of
+    # - initially produces d_w but with dimensions rearranged
+    d_w = tf.nn.convolution(
+        input=x_prepared,
+        filters=d_out_prepared,
+        strides=None,
+        padding='VALID')
+    return tf_NHWC_to_HWIO(d_w, reverse=True)
+
+
 def dilate(tensor, strides=None):
     """
     Inserts zeros between pixels in the spatial dimensions to simulate fractional strides.
@@ -49,19 +194,33 @@ def dilate(tensor, strides=None):
     return new_tensor
 
 
-def tf_NHWC_to_HWIO(out):
+def tf_NHWC_to_HWIO(out, reverse=False):
     """
-    Converts [batch, in_depth, in_height, in_width, in_channels]
-    to       [filter_depth, filter_height, filter_width, in_channels, out_channels]
+    Converts [batch, ..spatial_dims.., in_channels]
+    to       [..spatial_dims.., in_channels, out_channels],
+    by treating 'batch' as in_channels.
+    Args:
+      out: tensor
+      reverse: whether to reverse the operation. Ignored, because the implementation
+        is symmetric. But the argument makes the call-side intention clearer.
     """
-    return tf.transpose(out, perm=[1, 2, 3, 0, 4])
+    # eg: batch+2D+channels: tf.transpose(out, perm=[1, 2, 0, 3])
+    # eg: batch+3D+channels: tf.transpose(out, perm=[1, 2, 3, 0, 4])
+    shape = out.shape
+    axes = list(range(1, len(shape)-1)) + [0, len(shape)-1]
+    return tf.transpose(out, perm=axes)
 
 def tf_NHWC_to_CHWN(out):
     """
-    Converts [batch, in_depth, in_height, in_width, in_channels]
-    to       [in_channels, in_height, in_width, batch]
+    Flips the batch and channels dimensions.
+    Converts [batch, ..spatial_dims.., channels]
+    to       [in_channels, ..spatial_dims.., batch].
     """
-    return tf.transpose(out, perm=[4, 1, 2, 3, 0])
+    # eg: batch+2D+channels: tf.transpose(out, perm=[3, 1, 2, 0])
+    # eg: batch+3D+channels: tf.transpose(out, perm=[4, 1, 2, 3, 0])
+    shape = out.shape
+    axes = [len(shape)-1] + list(range(1, len(shape)-1)) + [0,]
+    return tf.transpose(out, perm=axes)
 
 
 def tf_crop(x, croppings):
--- conv_tools_test.py
+++ conv_tools_test.py
@@ -1,8 +1,11 @@
 from conv_tools import *
+import tensorflow as tf
+import tensorflow_probability as tfp
 
 
 def run_test_suite():
     dilate_test()
+    conv_backprop_filter_test()
     print("All conv_tools tests passed.")
 
 
@@ -38,3 +41,121 @@ def dilate_test():
     expected = np.tile(expected[np.newaxis, :, :, :, np.newaxis], reps=(32, 1, 1, 1, 6))
     actual = dilate(x, [2, 2, 2])
     assert np.allclose(actual, expected), f"Got: {actual}"
+
+
+def conv_backprop_filter_test():
+    def test_scenario(counters, A_0_shape, W_shape, strides, padding, description):
+        tf.random.set_seed(36)
+        A_0 = tf.random.normal(A_0_shape)
+        W = tf.random.normal(W_shape)
+
+        # forward pass plus ground-truth
+        with tf.GradientTape() as tape:
+            tape.watch(A_0)
+            tape.watch(W)
+            if len(A_0.shape) == 3:
+                Z = tf.nn.conv1d(A_0, W, stride=strides, padding=padding)
+            elif len(A_0.shape) == 4:
+                Z = tf.nn.conv2d(A_0, W, strides=strides, padding=padding)
+            elif len(A_0.shape) == 5:
+                # note: conv3d() strictly requires a list with len(strides) = number of dims
+                if not isinstance(strides, int) and len(strides) < 5:
+                    strides = [1] + strides + [1]
+                Z = tf.nn.conv3d(A_0, W, strides=strides, padding=padding)
+            else:
+                raise ValueError(f"Unsupported number of dims in A_0: {A_0.shape}")
+            # simulate being part of a network by computing mse loss against an 'expected_Z'
+            expected_Z = tf.random.normal(Z.shape)
+            loss = tf.reduce_mean(tf.square(Z - expected_Z))
+        dJdZ, true_dJdW = tape.gradient(loss, [Z, W])
+
+        count = counters[0]
+        counters[0] = count + 1
+
+        dJdW = conv_backprop_filter(A_0, dJdZ, W_shape, strides, padding)
+        if dJdW.shape != true_dJdW.shape:
+            print(f"[{count}] A_0: {A_0.shape}, W: {W.shape}, strides: {strides}, padding: {padding} -> "
+                  f"Z: {expected_Z.shape}")
+            raise ValueError(f"[{count}] {description}: got wrong shape for dJdW, expected {true_dJdW.shape}, "
+                             f"got: {dJdW.shape}")
+        else:
+            err = dJdW - true_dJdW
+            rmse = tf.sqrt(tf.reduce_mean(tf.square(err)))
+            if not np.allclose(dJdW, true_dJdW, atol=1e-07):
+                print(f"[{count}] A_0: {A_0.shape}, W: {W.shape}, strides: {strides}, padding: {padding} -> "
+                      f"Z: {expected_Z.shape}")
+                raise ValueError(f"[{count}] {description}: dJdW has incorrect values. Rmse={rmse}. "
+                                 f"Percentiles={tfp.stats.percentile(err, [0, 25, 50, 75, 100])}")
+
+    counters = [0]
+    # 1d
+    test_scenario(counters, A_0_shape=(32, 100, 5), W_shape=(3, 5, 16), strides=[1], padding='VALID',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 5), W_shape=(3, 5, 16), strides=[1], padding='SAME',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 4), W_shape=(3, 4, 16), strides=[1], padding='VALID',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 4), W_shape=(3, 4, 16), strides=[1], padding='SAME',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 5), W_shape=(3, 5, 16), strides=[2], padding='VALID',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 5), W_shape=(3, 5, 16), strides=[2], padding='SAME',
+                  description='Basic 2D')
+
+    # 2D
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 3, 5, 16), strides=[1, 1], padding='VALID',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 3, 5, 16), strides=[1, 1], padding='SAME',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 17, 16, 5), W_shape=(4, 4, 5, 16), strides=[1, 1], padding='VALID',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 17, 16, 5), W_shape=(4, 4, 5, 16), strides=[1, 1], padding='SAME',
+                  description='Basic 2D')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 3, 5, 16), strides=[2, 2], padding='VALID',
+                  description='2D with strides')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 3, 5, 16), strides=[2, 2], padding='SAME',
+                  description='2D with strides')
+    test_scenario(counters, A_0_shape=(32, 17, 16, 5), W_shape=(4, 4, 5, 16), strides=[2, 2], padding='VALID',
+                  description='2D with strides')
+    test_scenario(counters, A_0_shape=(32, 17, 16, 5), W_shape=(4, 4, 5, 16), strides=[2, 2], padding='SAME',
+                  description='2D with strides')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 4, 5, 16), strides=[1, 2], padding='VALID',
+                  description='Mixed 2D')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 4, 5, 16), strides=[2, 1], padding='SAME',
+                  description='Mixed 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 160, 8), W_shape=(3, 3, 8, 32), strides=[2, 2], padding='VALID',
+                  description='Large 2D')
+    test_scenario(counters, A_0_shape=(32, 100, 160, 8), W_shape=(3, 3, 8, 32), strides=[2, 2], padding='SAME',
+                  description='Large 2D')
+
+    # 3D
+    test_scenario(counters, A_0_shape=(32, 8, 9, 8, 5), W_shape=(3, 3, 4, 5, 16), strides=[1, 1, 1],
+                  padding='VALID', description='Basic 3D')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 8, 5), W_shape=(3, 3, 4, 5, 16), strides=[1, 1, 1],
+                  padding='SAME', description='Basic 3D')
+    test_scenario(counters, A_0_shape=(32, 17, 16, 17, 5), W_shape=(4, 3, 4, 5, 16), strides=[1, 1, 1],
+                  padding='VALID', description='Basic 3D')
+    test_scenario(counters, A_0_shape=(32, 17, 16, 17, 5), W_shape=(4, 3, 4, 5, 16), strides=[1, 1, 1],
+                  padding='SAME', description='Basic 3D')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 8, 5), W_shape=(3, 4, 4, 5, 16), strides=[2, 2, 2],
+                  padding='VALID', description='3D with strides')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 8, 5), W_shape=(3, 4, 4, 5, 16), strides=[2, 2, 2],
+                  padding='SAME', description='3D with strides')
+    test_scenario(counters, A_0_shape=(32, 7, 8, 9, 5), W_shape=(3, 3, 3, 5, 16), strides=[1, 2, 3],
+                  padding='VALID', description='3D with mixed strides')
+    test_scenario(counters, A_0_shape=(32, 7, 8, 9, 5), W_shape=(3, 3, 3, 5, 16), strides=[1, 2, 3],
+                  padding='VALID', description='3D with mixed strides')
+    test_scenario(counters, A_0_shape=(32, 7, 8, 9, 5), W_shape=(4, 4, 4, 5, 16), strides=[1, 2, 3],
+                  padding='VALID', description='3D with mixed strides')
+    test_scenario(counters, A_0_shape=(32, 7, 8, 9, 5), W_shape=(4, 4, 4, 5, 16), strides=[1, 2, 3],
+                  padding='VALID', description='3D with mixed strides')
+
+    # Argument forms
+    test_scenario(counters, A_0_shape=(32, 8, 5), W_shape=(3, 5, 16), strides=2, padding='VALID',
+                  description='Scalar strides')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 3, 5, 16), strides=2, padding='VALID',
+                  description='Scalar strides')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 5), W_shape=(3, 3, 5, 16), strides=[1, 1, 1, 1], padding='VALID',
+                  description='Full strides')
+    test_scenario(counters, A_0_shape=(32, 8, 9, 6, 5), W_shape=(3, 3, 3, 5, 16), strides=[1, 1, 1, 1, 1],
+                  padding='VALID', description='Full strides')
